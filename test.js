f173196617d6ce3fe7b36b995021c05ffd5e3db2
zhiyongzaixian <13852292775@163.com>
2019-03-04 15:42:18 +0800

    'xx'

---
+++ test.js
@@ -0,0 +1,12 @@
+function fun(...value) {
+  console.log(arguments);
+  console.log(value);
+}
+
+fun(11,22,33)
+
+
+let arr = [1,2,5,6,7,3,4]
+console.log(...arr); // iterator接口
+console.log([...arr].splice(0, 5));
+console.log(arr);
